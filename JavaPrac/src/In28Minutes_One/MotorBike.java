8086dfd4312a8424299340d60b1678a2db1c937d
Ranjan273 <thepradhan01@hotmail.com>
2022-02-20 18:42:09 +0530

    state object commits

--- JavaPrac/src/In28Minutes_One/MotorBike.java
+++ JavaPrac/src/In28Minutes_One/MotorBike.java
@@ -1,5 +1,13 @@
 package In28Minutes_One;
 
 public class MotorBike {
+	
+	//State
+	int speed;
+	
+	//Behaviour
+	void start() {
+		System.out.println("Bike started ");
+	}
 
 }
--- JavaPrac/src/In28Minutes_One/MotorBikeRunner.java
+++ JavaPrac/src/In28Minutes_One/MotorBikeRunner.java
@@ -3,8 +3,15 @@ package In28Minutes_One;
 public class MotorBikeRunner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		MotorBike ducati=new MotorBike();
+		MotorBike honda=new MotorBike();
+		
+		ducati.speed=100;
+		honda.speed=80;
+		
+		ducati.start();
+		honda.start();
 	}
 
 }
